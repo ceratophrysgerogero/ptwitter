0b8ab1f86ed0461aaf9ec42b769bcc0dc722abc2
ツノガエル <ceratophrysgerogero@gmail.com>
2019-11-12 11:08:23 +0900

    エラーメッセージ日本語化

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,11 @@ module Ptwitter
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
+    # エラーコメントを日本語にする
+    config.i18n.default_locale = :ja
+    # config/local以下のディレクトリ内にある全てのymlファイルを読み込む
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.yml').to_s]
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
