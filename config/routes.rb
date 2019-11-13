38161e0ad44ef323b0f5ff968a342109eb58552e
ツノガエル <ceratophrysgerogero@gmail.com>
2019-11-13 19:07:32 +0900

    session追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   root 'home_pages#home'
   resources :users
   get '/signup', to: 'users#new'
   post '/signup', to: 'users#create'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logaut', to: 'sessions#desroy'
 end
