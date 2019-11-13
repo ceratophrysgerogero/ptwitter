99b9d4df1ff749a111832aaab3664508e9fa9cb3
ツノガエル <ceratophrysgerogero@gmail.com>
2019-11-13 17:44:08 +0900

    有効なユーザー登録テスト

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -2,13 +2,27 @@ require 'test_helper'
 
 class UsersSignupTest < ActionDispatch::IntegrationTest
 
+  test "有効なユーザー登録" do
+    get signup_path
+    assert_difference 'User.count', 1 do
+      post users_path, params: {user: { name: "test",
+                                        email: "test@test.com",
+                                        password: "password",
+                                        password_confirmation: "password" }}
+    end
+    follow_redirect!
+    assert_template 'users/show'
+    assert_not flash.nil?
+  end
+   
+
   test "無効なユーザー登録" do
     get signup_path
     assert_no_difference 'User.count' do
       post users_path, params: {user: { name: "",
                                         email: "user@test",
                                         password: "pass",
-                                        password_confirmation: "bar" }}
+                                        password_confirmation: "pass" }}
     end
     assert_template 'users/new'
   end
