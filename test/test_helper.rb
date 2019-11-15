7bf59b45b83bcfdc410ff021d31a2576e96bd9e8
ツノガエル <ceratophrysgerogero@gmail.com>
2019-11-15 13:18:23 +0900

    テストのlogged_inメソッドをtest用に作成

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -9,4 +9,8 @@ class ActiveSupport::TestCase
   # Add more helper methods to be used by all tests here...
   require "minitest/reporters"
   Minitest::Reporters.use!
+
+  def is_logged_in?
+    !session[:user_id].nil?
+  end
 end
