4759480c46fdae51cfe0bf4abba58672b232500c
ツノガエル <ceratophrysgerogero@gmail.com>
2019-11-15 13:04:08 +0900

    有効なユーザー情報でログイン

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -2,6 +2,10 @@ require 'test_helper'
 
 class UsersLoginTest < ActionDispatch::IntegrationTest
 
+  def setup 
+    @user = users(:bobu)
+  end
+
   test "ログイン失敗時のflash" do
     get login_path
     assert_template 'sessions/new'
@@ -11,4 +15,18 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     get root_path
     assert flash.empty?
   end
+
+  test "有効なログイン" do
+    get login_path
+    post login_path, params: { session: { email: @user.email, password: 'password'} }
+    assert_redirected_to @user
+    follow_redirect!
+    assert_template 'users/show'   
+    assert_select "a[href=?]", login_path, count: 0
+    assert_select "a[href=?]", logout_path
+    assert_select "a[href=?]", user_path(@user)
+  end
 end
+
+
+
