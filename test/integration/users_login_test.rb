a44215bb15ea44e28de549d277297a48c37d82dc
ツノガエル <ceratophrysgerogero@gmail.com>
2019-11-16 11:15:21 +0900

    正常なログインからログアウトまでのテスト追加　テストに伴い間違っていたところを修正

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -26,6 +26,24 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", logout_path
     assert_select "a[href=?]", user_path(@user)
   end
+
+  test "正常なログインからログアウトまで" do
+    get login_path
+    post login_path, params: { session: { email: @user.email, password: 'password' }}
+    assert is_logged_in?
+    assert_redirected_to @user
+    follow_redirect!
+    assert_select "a[href=?]", login_path, count: 0
+    assert_select "a[href=?]", logout_path, count: 1
+    assert_select "a[href=?]", user_path(@user),  count: 1
+    delete logout_path
+    assert_not is_logged_in?
+    assert_redirected_to root_path
+    follow_redirect!
+    assert_select "a[href=?]", login_path, count: 1 
+    assert_select "a[href=?]", logout_path, count: 0
+    assert_select "a[href=?]", user_path(@user),  count: 0
+  end
 end
 
 
