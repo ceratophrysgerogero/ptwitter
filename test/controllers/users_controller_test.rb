2404dbcd13ec5d18527e647e62ade94a79a6d013
ツノガエル <ceratophrysgerogero@gmail.com>
2019-11-30 22:12:50 +0900

    test ログインしているユーザーではないユーザーが編集する

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -4,6 +4,7 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
 
   def setup
     @user = users(:bobu)
+    @other_user = users(:john)
   end
   
   test "ログインなしでユーザーを作成する" do
@@ -16,5 +17,15 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     patch user_path(@user), params: { user: { name: @user.name, email: @user.email }}
     assert_not flash.empty?
     assert_redirected_to login_url
+  end
+
+
+  test "ログインしているユーザーではないユーザーが編集する" do
+    log_in_as(@other_user)
+    get edit_user_path(@user)
+    patch user_path(@user), params: { user: { name: @user.name,
+                                              email: @user.email } }
+    assert flash.empty?
+    assert_redirected_to root_url
   end
 end
