ab4f1815328cc9538a309d6913729a49c8092dbe
ツノガエル <ceratophrysgerogero@gmail.com>
2019-12-07 19:02:34 +0900

    test "アドミンでログインしてアドミンユーザー以外のユーザーを一つ消す"   test "アドミンではないユーザーは削除するリンクが見えない" 追加

--- test/integration/users_index_test.rb
+++ test/integration/users_index_test.rb
@@ -2,16 +2,41 @@ require 'test_helper'
 
 class UsersIndexTest < ActionDispatch::IntegrationTest
   def setup
-    @user = users(:bobu)
+    @non_admin = users(:john)
+    @admin = users(:bobu)
   end
 
   test "ユーザー一覧ページネート機能" do
-    log_in_as(@user)
+    log_in_as(@non_admin)
     get users_path
     assert_template 'users/index'
     assert_select 'div.pagination'
     User.paginate(page: 1).each do |user|
       assert_select 'a[href=?]',user_path(user),text: user.name
     end
+  end
+
+  test "アドミンでログインしてアドミンユーザー以外のユーザーを一つ消す" do
+    log_in_as(@admin)
+    get users_path
+    assert_template 'users/index'
+    assert_select 'div.pagination'
+    
+    first_page_user = User.paginate(page:1)
+    first_page_user.each do |user|
+      assert_select 'a[href=?]', user_path(user), text:user.name
+      unless user == @adomin
+        assert_select 'a[href=?]', user_path(user), test: 'delete'
+      end
+    end
+    assert_difference 'User.count', -1 do
+      delete user_path(@non_admin)
+    end
+  end
+
+  test "アドミンではないユーザーは削除するリンクが見えない" do
+    log_in_as(@non_admin)
+    get users_path
+    assert_select 'a', text:'delete', count:0
   end
 end
