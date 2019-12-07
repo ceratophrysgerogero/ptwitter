a55cffd6acf94474fa4f20cb62964f2bb645f5b4
ツノガエル <ceratophrysgerogero@gmail.com>
2019-12-07 19:51:23 +0900

    マイクロポストのバリーデーションテスト

--- test/models/micropost_test.rb
+++ test/models/micropost_test.rb
@@ -1,7 +1,21 @@
 require 'test_helper'
 
 class MicropostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    @user = users(:bobu)
+    @micropost = @user.microposts.build(content: "テスト")
+  end
+
+  test "バリーデーション" do
+    assert @micropost.valid?
+  end
+
+  test "user_idが存在しなければいけない" do
+    @micropost.user_id =nil
+    assert_not @micropost.valid?
+  end
+
+
+
 end
