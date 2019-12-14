cb2e36647bf90ff6ce084570a647e0dc5264f1fa
ツノガエル <ceratophrysgerogero@gmail.com>
2019-12-14 17:58:36 +0900

    修正前のコードではヘッダーのuser_path(@user)に対してテストしていただけだったが新しいshowページではヘッダー以外にマイクロポスト一覧を表示させている部分にもuser_path(@user)を表示しているのでuser_path(@user)を数えるのではなくドロップメニューがあるかを確認するようにした

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -35,7 +35,7 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     follow_redirect!
     assert_select "a[href=?]", login_path, count: 0
     assert_select "a[href=?]", logout_path, count: 1
-    assert_select "a[href=?]", user_path(@user),  count: 1
+    assert_select "ul.dropdown-menu"
     delete logout_path
     assert_not is_logged_in?
     assert_redirected_to root_path
@@ -44,7 +44,7 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     follow_redirect!
     assert_select "a[href=?]", login_path, count: 1 
     assert_select "a[href=?]", logout_path, count: 0
-    assert_select "a[href=?]", user_path(@user),  count: 0
+    assert_select "ul.dropdown-menu", count:0
   end
 
   test "クッキーを使った永続的ログイン" do
