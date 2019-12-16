44b4c5e38443a518f1244880a5f1fcf1c18748f1
ツノガエル <ceratophrysgerogero@gmail.com>
2019-12-16 22:00:43 +0900

    マイクロポストルート追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get 'sessions/new'
   root 'home_pages#home'
   resources :users
+  resources :microposts, only: [:create, :destroy]
   get '/signup', to: 'users#new'
   post '/signup', to: 'users#create'
   get '/login', to: 'sessions#new'
