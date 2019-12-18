578e73ff3d7fa91a3516c1559ee8e9e05647ea92
ツノガエル <ceratophrysgerogero@gmail.com>
2019-12-18 19:37:07 +0900

    ユーザーだけではなくマイクロポストでも使用したいのでapplication_controllerに抜き出す

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,14 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include SessionsHelper
 
+  #ログインしてほしいところでログインチェックに使う
+  def logged_in_user
+    unless logged_in?
+      store_url
+      flash[:danger] ="ログインしてください"
+      redirect_to login_url
+    end
+  end
+
+
 end
