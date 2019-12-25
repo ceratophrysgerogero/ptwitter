6887393553290f915e93bb9cf92e6460308b1735
ツノガエル <ceratophrysgerogero@gmail.com>
2019-12-25 19:49:00 +0900

    マイクロポスト削除機能を追加

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,5 +1,6 @@
 class MicropostsController < ApplicationController
   before_action :logged_in_user, only: [:create, :destroy]
+  before_action :correct_to, only: destroy
 
 
   def create
@@ -8,16 +9,25 @@ class MicropostsController < ApplicationController
       flash[:success] = "マイクロポストを作成しました！！"
       redirect_to root_url
     else
+      @feed_itemes = []
       render 'home_pages/home'
     end
   end
 
   def destroy
+    @micrtopost.destroy
+    flash[:success] = "投稿を削除しました"
+    redirect_to request.referrer || root_url
   end
 
   private
 
   def microposts_strong_params
     params.require(:micropost).permit(:content)
+  end
+
+  def correct_user
+    @micropost = current_user.microposts.find_by(id: params[:id])
+    redirect_to root_url if @micropost.nil?
   end
 end
