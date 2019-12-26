a15a81bf56c0fe34fb444da07408c092574fe930
ツノガエル <ceratophrysgerogero@gmail.com>
2019-12-26 20:56:45 +0900

    タイポ

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,6 +1,6 @@
 class MicropostsController < ApplicationController
   before_action :logged_in_user, only: [:create, :destroy]
-  before_action :correct_to, only: destroy
+  before_action :correct_to, only: :destroy
 
 
   def create
