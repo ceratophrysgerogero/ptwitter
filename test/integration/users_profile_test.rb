e2e8aa5e5ceb6258b1de63102782a04f65b39937
ツノガエル <ceratophrysgerogero@gmail.com>
2019-12-27 20:41:51 +0900

    タイポ

--- test/integration/users_profile_test.rb
+++ test/integration/users_profile_test.rb
@@ -15,7 +15,7 @@ class UsersProfileTest < ActionDispatch::IntegrationTest
     assert_select 'div.pagination' 
     assert_match @user.microposts.count.to_s, response.body
     @user.microposts.paginate(page: 1).each do |micropost|
-      assert_mach micropost.content,response.body
+      assert_match micropost.content,response.body
     end
   end
 end
