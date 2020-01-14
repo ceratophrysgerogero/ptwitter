1d4c89e6358e8a514665e9335cfad2f70b3178cf
ツノガエル <ceratophrysgerogero@gmail.com>
2020-01-14 19:42:18 +0900

    relationshipのvalidatesテスト

--- test/models/relationship_test.rb
+++ test/models/relationship_test.rb
@@ -1,7 +1,23 @@
 require 'test_helper'
 
 class RelationshipTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    @relationship = Relationship.new(follower_id: users(:bobu).id,
+                                     followed_id: users(:archer).id)
+  end
+  
+  test "validできるか" do
+    assert @relationship.valid?
+  end
+
+  test "follower_idが必要" do
+    @relationship.follower_id = nil
+    assert_not @relationship.valid?
+  end
+
+  test "followed_idが必要" do
+    @relationship.followed_id = nil
+    assert_not @relationship.valid?
+  end
 end
