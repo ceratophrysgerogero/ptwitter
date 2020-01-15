774ebc39636dd2bb983dee10aa0e73e32d08226c
ツノガエル <ceratophrysgerogero@gmail.com>
2020-01-15 18:47:54 +0900

    リレーションシップのアクセス制限テスト実装（くりあー)

--- test/controllers/relationships_controller_test.rb
+++ test/controllers/relationships_controller_test.rb
@@ -1,7 +1,19 @@
 require 'test_helper'
 
 class RelationshipsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "作成にユーザーログインが必要"do
+      assert_no_difference 'Relationship.count' do
+      post relationships_path
+    end
+    assert_redirected_to login_url
+  end
+
+
+  test "削除にユーザーログインが必要" do
+    assert_no_difference 'Relationship.count' do
+      delete relationship_path(relationships(:one))
+    end
+    assert_redirected_to login_url
+  end
 end
