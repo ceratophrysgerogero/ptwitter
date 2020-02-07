534b0b316efe80baff93f2b731fc1c4edd63b2bc
ツノガエル <ceratophrysgerogero@gmail.com>
2020-02-07 11:17:50 +0900

    投稿画像サイズ変更

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -2,6 +2,7 @@ class PictureUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
   include CarrierWave::MiniMagick
+  process resize_to_limit: [400,400]
 
   # Choose what kind of storage to use for this uploader:
   storage :file
@@ -22,7 +23,7 @@ class PictureUploader < CarrierWave::Uploader::Base
   # end
 
   # Process files as they are uploaded:
-  process resize_to_fit: [200, 300]
+  # process resize_to_fit: [200, 300]
   #
   # def scale(width, height)
      # do something
@@ -30,7 +31,7 @@ class PictureUploader < CarrierWave::Uploader::Base
 
   # Create different versions of your uploaded files:
   version :thumb do
-     process resize_to_fit: [200, 200]
+   #  process resize_to_fit: [200, 200]
   end
 
   # Add a white list of extensions which are allowed to be uploaded.
