3eb58e2e3bdac7e83dee89a73e04f6bb52a19226
ツノガエル <ceratophrysgerogero@gmail.com>
2020-02-25 16:44:48 +0900

    どのユーザーを見ても自分のform-forを表示するようにした。マイクロポストを生成したとき現在のURLに遷移するようにした

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -2,12 +2,11 @@ class MicropostsController < ApplicationController
   before_action :logged_in_user, only: [:create, :destroy]
   before_action :correct_user, only: :destroy
 
-
   def create
     @micropost = current_user.microposts.build(microposts_strong_params)
     if @micropost.save 
       flash[:success] = "マイクロポストを作成しました！！"
-      redirect_to user_path(current_user)
+      redirect_to user_path(params[:params_id])
     else
       @feed_items = []
       render 'home_pages/home'
