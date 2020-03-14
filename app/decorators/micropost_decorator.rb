3c27ab895c9cae06c9af51d82889002035390eba
ツノガエル <ceratophrysgerogero@gmail.com>
2020-03-14 16:58:19 +0900

    呟き機能をマークダウン 方式で書いてシンタックスハイライトで表示する機能をついか

--- app/decorators/micropost_decorator.rb
+++ app/decorators/micropost_decorator.rb
@@ -3,9 +3,9 @@
 module MicropostDecorator
   def decorated_content
     if content_object.reply?
-      link_to("@#{in_reply_to.name}", in_reply_to) + " " + content_object.content
+      link_to("@#{in_reply_to.name}", in_reply_to) + " " + markdown(content_object.content).html_safe
     else
-      content
+      markdown(content).html_safe
     end
   end
 
